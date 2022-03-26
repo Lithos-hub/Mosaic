66c6dc10603a60417f95bda2e9c098ba7d2ccce3
unknown <lithos.contact@gmail.com>
2022-03-26 09:48:24 +0100

    Working on send and backspace buttons

--- src/components/Keyboard.vue
+++ src/components/Keyboard.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="keyboard__wrapper">
+    <div class="keyboard__letters">
     <div class="keyboard__row">
       <KeyboardButton
         v-for="(letter, i) in keyboardKeys1"
@@ -23,6 +24,15 @@
         :letter="letter"
         @emit-key="emitKey"
       />
+    </div>
+    </div>
+    <div class="keyboard__options">
+      <button class="keyboard__backspace--btn">
+        <mdicon name="close-box" size="40" />
+      </button>
+      <button class="keyboard__send--btn">
+        <mdicon name="send" size="40" />
+      </button>
     </div>
   </div>
 </template>
@@ -48,12 +58,36 @@ const emitKey = (key) => {
 <style lang="scss" scoped>
 @import "../scss/variables";
 
-.keyboard__wrapper {
-
-}
-
 .keyboard__row {
   display: flexbox;
   padding: 5px;
+}
+
+.keyboard__options {
+  position: absolute;
+  right: 30vw;
+  top: 50%;
+  transform: translateY(-50%);
+}
+
+.keyboard__backspace--btn, .keyboard__send--btn {
+    display: block;
+    cursor: pointer;
+    transition: all .3s ease-out;
+    font-weight: bold;
+    color: $mainDark;
+    background: rgba(0, 0, 0, 0.253);
+    border-radius: 5px;
+    border: none;
+    margin-block: 1em;
+    width: 50px;
+    max-width: 50px;
+    height: 50px;
+    max-height: 50px;
+
+    &:hover {
+        background: $mainDark;
+        color: white;
+    }
 }
 </style>
